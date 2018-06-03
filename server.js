bd35650acb4bb6e7eff0784659a81c9487141f08
Ashley <ashleymatthews91@gmail.com>
2018-06-03 22:48:37 +0100

    Serves app from new endpoint and adds static files to express static config to allow app and api to run on same service.

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 
 const competitionService = require('./services/competitionService');
 const dbService = require('./services/dbService');
@@ -7,7 +8,9 @@ const playerService = require('./services/playerService');
 const server = express();
 
 server.set('port', (process.env.PORT || 5000));
-server.use(express.static('build'));
+
+server.use('/app', express.static(path.join(__dirname, 'build')));
+server.use('/static', express.static(path.join(__dirname, 'build/static')));
 
 server.get('/api/players', async (request, response) => {
     const playerData = await playerService.getAllPlayerData();

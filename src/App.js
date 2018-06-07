70ba2b6cf8c3b471ed8362d66ad4fbc8a3a04899
Ashley Matthews <>
2018-06-07 11:07:55 +0100

    Adds precision to prize pools

--- src/App.js
+++ src/App.js
@@ -42,8 +42,8 @@ class App extends Component {
     getPrizePool(players) {
         const totalCash = players.length * 3;
         return {
-            first: totalCash * 0.85,
-            last: totalCash * 0.15
+            first: (totalCash * 0.85).toPrecision(2),
+            last: (totalCash * 0.15).toPrecision(2)
         };
     };
 

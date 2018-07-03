8969674857bbcde6ec9547aee5ce445cd4d0f11d
Craig Ayre <craigayre@live.co.uk>
2018-07-03 08:27:44 +0100

    Grey out players that have no teams remaining

--- src/components/PlayerTable.jsx
+++ src/components/PlayerTable.jsx
@@ -5,15 +5,30 @@ import Table from './Table';
 const headings = ['Name', 'Goal Teams', 'Outcome Teams', 'Predicted Total Goals', 'Total Points'];
 
 class PlayerTable extends Component {
+    isTeamEliminated = (team) => {
+        return this.props.teams[team].isEliminated;
+    }
+
     renderTeams(teams) {
         return teams.map(team => (
-            this.props.teams[team].isEliminated ? <s>{team}</s> : team
+            this.isTeamEliminated(team) ? <s>{team}</s> : team
         )).reduce((prev, curr) => [prev, ', ', curr])
     }
 
+    areAllPlayerTeamsEliminated({ goals, outcomes }) {
+        const areAllTeamsEliminated = teams => teams.every(this.isTeamEliminated)
+
+        return areAllTeamsEliminated(goals) && areAllTeamsEliminated(outcomes)
+    }
+
     renderRow = (row) => {
+        const className = this.areAllPlayerTeamsEliminated(row.teams) ? 'team-eliminated' : ''
+
         return (
-            <tr key={row.name}>
+            <tr
+                key={row.name}
+                className={className}
+            >
                 <td>{row.name}</td>
                 <td>{this.renderTeams(row.teams.goals)}</td>
                 <td>{this.renderTeams(row.teams.outcomes)}</td>

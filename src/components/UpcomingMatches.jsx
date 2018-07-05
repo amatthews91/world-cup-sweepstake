a32bd439dd69da0818598cbcb8f7f51170beefae
Ashley Matthews <>
2018-07-05 13:52:20 +0100

    Adds currently unused component to display matches which will be played today, need a server update to send these down before being used.

---
+++ src/components/UpcomingMatches.jsx
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import moment from 'moment';
+
+import './UpcomingMatches.css';
+
+class UpcomingMatches extends Component {
+
+  getMatchTime(date) {
+    return moment(date).format('HH:mm');
+  }
+
+  getMatchStatus(match) {
+    switch (match.status) {
+      case 'IN_PLAY':
+        return <span>Live</span>;
+      case 'FINISHED':
+        return <span>FT</span>;
+      default:
+        return <span></span>;
+    }
+  }
+
+  renderRow(match) {
+    return(
+      <div className="match-row">
+        <div className='match-result'>
+          <div className="match-team match-team-home">{match.homeTeamName}</div>
+          { match.status === 'IN_PLAY' || match.status === 'FINISHED' ?
+            <div className="match-score">
+              <div className="match-goals">
+                {match.result.homeTeamGoals}
+              </div>
+              <div className="match-goals">
+                {match.result.awayTeamGoals}
+              </div>
+            </div> :
+            <div className="match-time">{this.getMatchTime(match.date)}</div>
+          }
+          <div className="match-team match-team-away">{match.awayTeamName}</div>
+        </div>
+        <div className="match-status">{this.getMatchStatus(match)}</div>
+      </div>
+    );
+  }
+
+  render() {
+    return (
+      <div className="upcoming-matches">
+        <h2>Today's Matches</h2>
+        <div className="match-info">
+          { this.props.matches.map(match => this.renderRow(match)) }
+        </div>
+      </div>
+    );
+  }
+
+}
+
+export default UpcomingMatches;
